3fa84d7a9d3b9a7617d32b9f9d3ff792482e4861
Meganandhitha <151141835+Meganandhitha@users.noreply.github.com>
2026-01-13 21:06:36 +0530

    Create Kth Largest Element.py

---
+++ 11.01.2026/Kth Largest Element.py
@@ -0,0 +1,12 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.q = []
+        self.size = k
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        heappush(self.q, val)
+        if len(self.q) > self.size:
+            heappop(self.q)
+        return self.q[0]
