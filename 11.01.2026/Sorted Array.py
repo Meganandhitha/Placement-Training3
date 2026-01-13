4ed5d8f38c0503a9355194ec3d2754fed112c592
Meganandhitha <151141835+Meganandhitha@users.noreply.github.com>
2026-01-13 21:05:51 +0530

    Create Sorted Array.py

---
+++ 11.01.2026/Sorted Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, 20000
+        while left < right:
+            mid = (left + right) >> 1
+            if reader.get(mid) >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left if reader.get(left) == target else -1
